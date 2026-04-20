a4341be97e66cd547821b8f38ce62a616873bf70
manu <emmanuel@familleberte.com>
2026-04-21 01:21:51 +0200

    Initial commit

---
+++ src/test/java/org/medilabo/screening/ScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medilabo.screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
