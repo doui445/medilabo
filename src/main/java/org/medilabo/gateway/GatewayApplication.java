a0685f4e81de72595c7a79f4e1bd18ac22bffa85
manu <emmanuel@familleberte.com>
2026-04-22 17:27:50 +0200

    clean for dockerize

--- src/main/java/org/medilabo/gateway/GatewayApplication.java
+++ src/main/java/org/medilabo/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package org.medilabo.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -8,6 +9,16 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+	@Value("${patient.api.url}")
+	private String patientUrl;
+
+	@Value("${notes.api.url}")
+	private String notesUrl;
+
+	@Value("${screening.api.url}")
+	private String screeningUrl;
+
 
 	static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -18,13 +29,13 @@ public class GatewayApplication {
 		return builder.routes()
 				.route("patient", predicateSpec -> predicateSpec
 						.path("/api/patients/**")
-						.uri("http://localhost:8081"))
+						.uri(patientUrl))
 				.route("notes", predicateSpec -> predicateSpec
 						.path("/api/notes/**")
-						.uri("http://localhost:8082"))
+						.uri(notesUrl))
 				.route("screening", predicateSpec -> predicateSpec
 						.path("/api/screening/**")
-						.uri("http://localhost:8083"))
+						.uri(screeningUrl))
 				.build();
 	}
 }
